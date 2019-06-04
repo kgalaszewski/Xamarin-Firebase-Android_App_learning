c74cb92c163c89c8ef6eb28d11a472790e2320de
Karol Galaszewski <kgalaszewski95@gmail.com>
2019-06-04 16:51:24 +0200

    Project for ug Xamarin android with Cloud Firebase Firestore

---
+++ App1sss/Adapters/DataAdapter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+using App1sss;
+using UGAndroidCloud.Models;
+
+namespace UGAndroidCloud.Adapters
+{
+    class DataAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<DataAdapterClickEventArgs> ItemClick;
+        public event EventHandler<DataAdapterClickEventArgs> ItemLongClick;
+        List<InfoModel> infosList;
+
+        public DataAdapter(List<InfoModel> data)
+        {
+            infosList = data;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = null;
+
+            itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.row, parent, false);
+            var vh = new DataAdapterViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = infosList[position];
+
+            if (item != null && viewHolder != null)
+            {
+                var holder = viewHolder as DataAdapterViewHolder;
+                holder.TypeText.Text = !string.IsNullOrWhiteSpace(item.Type) ? item.Type : "";
+                holder.BreedText.Text = !string.IsNullOrWhiteSpace(item.Breed) ? item.Breed : "";
+                holder.NameText.Text = !string.IsNullOrWhiteSpace(item.Name) ? item.Name : "";
+                holder.InfoText.Text = !string.IsNullOrWhiteSpace(item.Info) ? item.Info : "";
+            }
+        }
+
+        public override int ItemCount => infosList.Count;
+
+        void OnClick(DataAdapterClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(DataAdapterClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+    public class DataAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView TypeText { get; set; }
+        public TextView BreedText { get; set; }
+        public TextView NameText { get; set; }
+        public TextView InfoText { get; set; }
+
+
+        public DataAdapterViewHolder(View itemView, Action<DataAdapterClickEventArgs> clickListener,
+                            Action<DataAdapterClickEventArgs> longClickListener) : base(itemView)
+        {
+            TypeText = (TextView)ItemView.FindViewById(Resource.Id.Type);
+            BreedText = (TextView)ItemView.FindViewById(Resource.Id.Breed);
+            NameText = (TextView)ItemView.FindViewById(Resource.Id.Name);
+            InfoText = (TextView)ItemView.FindViewById(Resource.Id.Info);
+
+            itemView.Click += (sender, e) => clickListener(new DataAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new DataAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class DataAdapterClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ App1sss/MainActivity.cs
@@ -0,0 +1,127 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Firebase.Firestore;
+using Firebase;
+using Java.Util;
+using Android.Content;
+using System.Threading.Tasks;
+using Android.Gms.Tasks;
+using Java.Lang;
+using UGAndroidCloud.Models;
+using System.Collections.Generic;
+using Android.Support.V7.Widget;
+using UGAndroidCloud.Adapters;
+using App1sss;
+
+namespace UGAndroidCloud
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnSuccessListener
+    {
+        EditText Type;
+        EditText Breed;
+        EditText Name;
+        EditText Info;
+        Button saveButton;
+        RecyclerView recyclerView;
+
+        DataAdapter dataAdapter;
+
+        FirebaseFirestore database;
+        List<InfoModel> listOfInfos = new List<InfoModel>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            ConnectViews();
+            database = GetDataBase();
+
+            FetchData();
+            SetuprecyclerView();
+        }
+
+        void ConnectViews()
+        {
+            Type = (EditText)FindViewById(Resource.Id.Type);
+            Breed = (EditText)FindViewById(Resource.Id.Breed);
+            Name = (EditText)FindViewById(Resource.Id.Name);
+            Info = (EditText)FindViewById(Resource.Id.Info);
+            saveButton = (Button)FindViewById(Resource.Id.saveButton);
+            recyclerView = (RecyclerView)FindViewById(Resource.Id.dataRecyclerView);
+
+            saveButton.Click += SaveButton_Click;
+        }
+
+        void FetchData()
+        {
+            database.Collection("Infos").Get().AddOnSuccessListener(this);
+        }
+
+        void SetuprecyclerView()
+        {
+            recyclerView.SetLayoutManager(new LinearLayoutManager(recyclerView.Context));
+            dataAdapter = new DataAdapter(listOfInfos);
+            recyclerView.SetAdapter(dataAdapter);
+        }
+
+        private void SaveButton_Click(object sender, System.EventArgs e)
+        {
+            HashMap map = new HashMap();
+            map.Put("Type", Type.Text);
+            map.Put("Breed", Breed.Text);
+            map.Put("Name", Name.Text);
+            map.Put("Info", Info.Text);
+
+            DocumentReference docRef = database.Collection("Infos").Document();
+            docRef.Set(map);
+        }
+
+        public FirebaseFirestore GetDataBase()
+        {
+            FirebaseFirestore database;
+
+            var options = new FirebaseOptions.Builder().SetProjectId("ugandroidcloudwikiprojectt")
+                .SetApplicationId("ugandroidcloudwikiprojectt")
+                .SetApiKey("AIzaSyCBm-SJ2o0O-CmpdsaIwwKlkVnx3zAYwRM")
+                .SetDatabaseUrl("https://ugandroidcloudwikiprojectt.firebaseio.com")
+                .SetStorageBucket("ugandroidcloudwikiprojectt.appspot.com").
+                Build();
+
+            var app = FirebaseApp.InitializeApp(this, options);
+            database = FirebaseFirestore.GetInstance(app);
+
+            return database;
+        }
+
+        public void OnSuccess(Object result)
+        {
+            var snapshot = (QuerySnapshot)result;
+
+            if (!snapshot.IsEmpty)
+            {
+                var documents = snapshot.Documents;
+
+                listOfInfos.Clear();
+                foreach (var item in documents)
+                {
+                    InfoModel info = new InfoModel();
+
+                    string typeToSet = item.Get("Type") != null ? item.Get("Type").ToString() : "default Type";
+                    info.Type = $"Type : {typeToSet}";
+                    string breedToSet = item.Get("Breed") != null ? item.Get("Breed").ToString() : "default Breed";
+                    info.Breed = $"Breed : {breedToSet}";
+                    string nameToSet = item.Get("Name") != null ? item.Get("Name").ToString() : "default Name";
+                    info.Name = $"Name : {nameToSet}";
+                    string InfoToSet = item.Get("Info") != null ? item.Get("Info").ToString() : "default Info";
+                    info.Info = $"Info : {InfoToSet}";
+
+                    listOfInfos.Add(info);
+                }
+            }
+        }
+    }
+}
